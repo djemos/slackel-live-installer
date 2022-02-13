52d1c7e51d5a6575f9e55162adc32f5455e305eb
Dimitris Tzemos <djemos@users.noreply.github.com>
2022-02-13 19:12:23 +0200

    Update maketxz.sh

--- maketxz.sh
+++ maketxz.sh
@@ -5,7 +5,7 @@ mkdir -p pkg
 export DESTDIR=$PWD/pkg
 VER=1.2.5
 ARCH=${ARCH:-x86_64}
-RLZ=3dj
+RLZ=8dj
 
 	cmake -DCMAKE_INSTALL_PREFIX=/usr .
 
@@ -17,15 +17,10 @@ RLZ=3dj
 	mkdir install
 
 cat <<EOF > install/slack-desc
-sli: "Slackware Live (live DVD/USB/NFS build and install tool)"
-sli: "It is written for Slackware / Slackware64 / Slackware-ARM Linux:"
-sli: "support GPT and legacy MBR partitionned disks, UEFI and CSM "
-sli: "(legacy / BIOS) booting, contains all the necessary to convert an"
-sli: "installed system to a live system and vice-versa;"
-sli: "it doesn’t need any kernel recompile, but it support seamlessly"
-sli: "AUFS if your kernel support them; no changes are needed on the system"
-sli: "to make live; the live system boots like an installed one"
-sli: "(100% compatible with stock Slackware); support persistent changes"
+sli: Slackel Live Installer(gui application for installing live system)"
+sli: "It is  written for Slackel Linux:"
+sli: "support GPT and legacy MBR partitionned disks, UEFI and CSM"
+sli: "(legacy / BIOS) booting"
 EOF
 
 	/sbin/makepkg -l y -c n ../sli-$VER-$ARCH-$RLZ.txz
